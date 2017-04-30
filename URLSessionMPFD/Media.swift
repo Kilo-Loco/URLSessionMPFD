6ddef9ddebdaaac2fbb79046e62a0b9d03513604
Kyle <kiloloco@me.com>
2017-04-29 18:07:26 -0700

    finished tutorial and fixed bug too

---
+++ URLSessionMPFD/Media.swift
@@ -0,0 +1,26 @@
+//
+//  Media.swift
+//  URLSessionMPFD
+//
+//  Created by Kyle Lee on 4/29/17.
+//  Copyright © 2017 Kyle Lee. All rights reserved.
+//
+
+import UIKit
+
+struct Media {
+    let key: String
+    let filename: String
+    let data: Data
+    let mimeType: String
+    
+    init?(withImage image: UIImage, forKey key: String) {
+        self.key = key
+        self.mimeType = "image/jpeg"
+        self.filename = "kyleleeheadiconimage234567.jpg"
+        
+        guard let data = UIImageJPEGRepresentation(image, 0.7) else { return nil }
+        self.data = data
+    }
+    
+}
---
+++ URLSessionMPFD/ViewController.swift
@@ -0,0 +1,128 @@
+//
+//  ViewController.swift
+//  URLSessionMPFD
+//
+//  Created by Kyle Lee on 4/29/17.
+//  Copyright © 2017 Kyle Lee. All rights reserved.
+//
+
+import UIKit
+
+typealias Parameters = [String: String]
+
+class ViewController: UIViewController {
+    
+
+    @IBAction func getRequest(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        var request = URLRequest(url: url)
+        
+        let boundary = generateBoundary()
+        
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        let dataBody = createDataBody(withParameters: nil, media: nil, boundary: boundary)
+        request.httpBody = dataBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
+        
+    }
+    
+    
+    @IBAction func postRequest(_ sender: Any) {
+        
+        let parameters = ["name": "MyTestFile123321",
+                          "description": "My tutorial test file for MPFD uploads"]
+        
+        guard let mediaImage = Media(withImage: #imageLiteral(resourceName: "testImage"), forKey: "image") else { return }
+        
+        guard let url = URL(string: "https://api.imgur.com/3/image") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let boundary = generateBoundary()
+        
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        request.addValue("Client-ID f65203f7020dddc", forHTTPHeaderField: "Authorization")
+        
+        let dataBody = createDataBody(withParameters: parameters, media: [mediaImage], boundary: boundary)
+        request.httpBody = dataBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+            }.resume()
+    }
+    
+    func generateBoundary() -> String {
+        return "Boundary-\(NSUUID().uuidString)"
+    }
+    
+    func createDataBody(withParameters params: Parameters?, media: [Media]?, boundary: String) -> Data {
+        
+        let lineBreak = "\r\n"
+        var body = Data()
+        
+        if let parameters = params {
+            for (key, value) in parameters {
+                body.append("--\(boundary + lineBreak)")
+                body.append("Content-Disposition: form-data; name=\"\(key)\"\(lineBreak + lineBreak)")
+                body.append("\(value + lineBreak)")
+            }
+        }
+        
+        if let media = media {
+            for photo in media {
+                body.append("--\(boundary + lineBreak)")
+                body.append("Content-Disposition: form-data; name=\"\(photo.key)\"; filename=\"\(photo.filename)\"\(lineBreak)")
+                body.append("Content-Type: \(photo.mimeType + lineBreak + lineBreak)")
+                body.append(photo.data)
+                body.append(lineBreak)
+            }
+        }
+        
+        body.append("--\(boundary)--\(lineBreak)")
+        
+        return body
+    }
+    
+}
+
+
+extension Data {
+    mutating func append(_ string: String) {
+        if let data = string.data(using: .utf8) {
+            append(data)
+        }
+    }
+}
+
+
+
+
+
